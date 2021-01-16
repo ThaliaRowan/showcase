ea02152214e15a852cdf4ea283ecc965b4c44973
SAMIA <68030713+lorjoh@users.noreply.github.com>
2021-01-16 11:57:15 -0500

    jumbo update

--- client/src/components/jumboContainer.js
+++ client/src/components/jumboContainer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function JumboContainer() {
   return (
-      <div className="jumbotron container-fluid mt-2 text-center p-2" style="background-color:#404040 ;color: white;">
-        <h1> <strong>Showcase</strong></h1>
+      <div className="jumbotron container-fluid mt-2 text-center p-2" >   
+      <h1> <strong>Showcase</strong></h1>
       </div>
      
   );
@@ -11,3 +11,4 @@ function JumboContainer() {
 
 
 export default JumboContainer;
+// style={"background-color:#404040", "color: white"}
