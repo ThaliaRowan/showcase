a06a24e9b25061077a5aa692ecdbb6cf35112eb2
SAMIA <68030713+lorjoh@users.noreply.github.com>
2021-01-21 12:46:33 -0500

    2nd rearrangement

---
+++ client/src/components/tab.js
@@ -0,0 +1,30 @@
+// import React from "react";
+
+
+
+
+// function Tab(props) {
+//     return (
+//         <div>
+//         <button type="button" className="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+//   Most Searched:{props.mostsearchedUser}
+// </button>
+
+// <button type="button" className="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+//   Least Searched
+// </button>
+
+// <button type="button" className="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
+// sagittis lacus vel augue laoreet rutrum faucibus.">
+//   Popover on bottom
+// </button>
+
+// <button type="button" className="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+//   Popover on left
+// </button>
+// </div>
+//     );
+// }
+
+
+// export default Tab;
